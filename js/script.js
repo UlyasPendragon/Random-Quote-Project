7198849d500c0353934a03bc85da69aab8b85cc3
UlyasPendragon <74525241+UlyasPendragon@users.noreply.github.com>
2021-12-05 03:34:55 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,31 +3,60 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// For assistance:
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+/***
+ * `quotes` array
+ ***/
 
-
+const quotes = [
+  {
+    quote: "1",
+    source: "1",
+    citation: "1",
+    year: "1",
+  },
+  {
+    quote: "2",
+    source: "2",
+    citation: "2",
+    year: "2",
+  },
+  {
+    quote: "3",
+    source: "3",
+    citation: "3",
+    year: "3",
+  },
+  {
+    quote: "4",
+    source: "4",
+    citation: "4",
+    year: "4",
+  },
+  {
+    quote: "5",
+    source: "5",
+    citation: "5",
+    year: "5",
+  },
+];
 
 /***
  * `getRandomQuote` function
-***/
-
-
+ ***/
 
 /***
  * `printQuote` function
-***/
-
-
+ ***/
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document
+  .getElementById("load-quote")
+  .addEventListener("click", printQuote, false);
